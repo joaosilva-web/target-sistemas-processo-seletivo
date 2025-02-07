989b720cb8488133ea994b24c7ab730d3c4b8fd6
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-07 01:35:01 -0300

    fourth question solved

--- index.js
+++ index.js
@@ -1,116 +1,178 @@
 function firstQuestion() {
-    let INDICE = 13;
-    let SOMA = 0;
-    let K = 0;
+  let INDICE = 13;
+  let SOMA = 0;
+  let K = 0;
 
-    while (K < INDICE) {
-        K = K + 1;
-        SOMA = SOMA + K;
-    }
+  while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+  }
 
-
-    return SOMA;
+  return SOMA;
 }
 
 // Exibir o resultado no HTML
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("resultado").innerText = "Portanto o resultado é: " + firstQuestion();
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("resultado").innerText =
+    "Portanto o resultado é: " + firstQuestion();
 });
 
 // Segunda questão
-document.getElementById("fibonacciForm").addEventListener("submit", function(event) {
+document
+  .getElementById("fibonacciForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio do formulário e o recarregamento da página
 
     const inputElement = document.getElementById("inputNumberToCalcFibonacci");
     const valueToCalc = inputElement.value;
 
-    calcExistInFibonacci(valueToCalc)
-});
+    calcExistInFibonacci(valueToCalc);
+  });
 
 function calcExistInFibonacci(valueReceived) {
-        let prevValue = 0;
-        let currentValue = 1;
-        let fibonacci = 0;
-        let formattedValueReceived = parseInt(valueReceived)
-      
-        while (fibonacci <= formattedValueReceived) {
-          fibonacci = prevValue + currentValue;
-          prevValue = currentValue;
-          currentValue = fibonacci;
-      
-          if (formattedValueReceived === fibonacci || formattedValueReceived === 0) {
-            document.getElementById("secondQuestionResult").innerText = "O número informado pertence à sequência de Fibonacci";
-            return;
-          }
-      
-          if (fibonacci > formattedValueReceived) {
-            document.getElementById("secondQuestionResult").innerText = "O número informado não pertence à sequência de Fibonacci";
-            return;
-          }
-        }
+  let prevValue = 0;
+  let currentValue = 1;
+  let fibonacci = 0;
+  let formattedValueReceived = parseInt(valueReceived);
 
+  while (fibonacci <= formattedValueReceived) {
+    fibonacci = prevValue + currentValue;
+    prevValue = currentValue;
+    currentValue = fibonacci;
+
+    if (formattedValueReceived === fibonacci || formattedValueReceived === 0) {
+      document.getElementById("secondQuestionResult").innerText =
+        "O número informado pertence à sequência de Fibonacci";
+      return;
     }
 
-    //third question
-
-    function filterData(dataReceived){//Function to remove All useless days
-       const dataWithoutUselessDays = dataReceived.filter(data => data.valor !== 0);
-       return dataWithoutUselessDays;
+    if (fibonacci > formattedValueReceived) {
+      document.getElementById("secondQuestionResult").innerText =
+        "O número informado não pertence à sequência de Fibonacci";
+      return;
     }
+  }
+}
 
-    function getExtremesProfit(dataReceived){
-        let highestProfit = 0;
-        let lowestProfit = 0
-        dataReceived.map(data => {
-            if(data.valor > highestProfit) {
-                highestProfit = data.valor;
-            }
-        })
+//third question
+function filterData(dataReceived) {
+  //Function to remove All useless days
+  const dataWithoutUselessDays = dataReceived.filter(
+    (data) => data.valor !== 0
+  );
+  return dataWithoutUselessDays;
+}
 
-        lowestProfit = highestProfit; //set the lowest profit as the highest profit to start with the highest possible value parameter
-        dataReceived.map(data => {
-            if(data.valor <= lowestProfit){
-                lowestProfit = data.valor;
-            }
-        })
-        return {highestProfit, lowestProfit}
+function getExtremesProfit(dataReceived) {
+  let highestProfit = 0;
+  let lowestProfit = 0;
+  dataReceived.map((data) => {
+    if (data.valor > highestProfit) {
+      highestProfit = data.valor;
     }
+  });
 
-    function getAboveAverageProfitDays(dataReceived) {
-        const daysToCalcAverage = dataReceived.length;
-        let sumProfit = 0
-        let averageProfit = 0;
-        console.log(dataReceived)
-        for(let i = 0; i < daysToCalcAverage; i++) {
-            sumProfit += dataReceived[i].valor;
-        }
-
-        averageProfit = sumProfit / daysToCalcAverage;
-
-        const aboveAverageProfitDays = dataReceived.filter(data => data.valor >= averageProfit);
-
-        return aboveAverageProfitDays.length;
+  lowestProfit = highestProfit; //set the lowest profit as the highest profit to start with the highest possible value parameter
+  dataReceived.map((data) => {
+    if (data.valor <= lowestProfit) {
+      lowestProfit = data.valor;
     }
+  });
+  return { highestProfit, lowestProfit };
+}
 
-    fetch("./dados.json")
-    .then(response => response.json())
-    .then(data => {
-       const newData = filterData(data);
+function getAboveAverageProfitDays(dataReceived) {
+  const daysToCalcAverage = dataReceived.length;
+  let sumProfit = 0;
+  let averageProfit = 0;
+  console.log(dataReceived);
+  for (let i = 0; i < daysToCalcAverage; i++) {
+    sumProfit += dataReceived[i].valor;
+  }
 
-       const extremeProfits = getExtremesProfit(newData);
-    
+  averageProfit = sumProfit / daysToCalcAverage;
 
-       const aboveAverageProfitDays = getAboveAverageProfitDays(newData)
-       document.getElementById("highestProfit").innerText = `O maior Faturamento foi de ${extremeProfits.highestProfit.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
-       document.getElementById("lowestProfit").innerText = `O menor Faturamento foi de ${extremeProfits.lowestProfit.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
-       document.getElementById("aboveAverageProfitDays").innerText = `A quantidade de dias que tiveram um lucro maior do que a média mensal foram: ${aboveAverageProfitDays}`;
+  const aboveAverageProfitDays = dataReceived.filter(
+    (data) => data.valor >= averageProfit
+  );
 
+  return aboveAverageProfitDays.length;
+}
 
-    })
-    .catch(error => {
-        console.error('Erro ao ler o arquivo JSON:', error);
-    })
+fetch("./dados.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const newData = filterData(data);
+
+    const extremeProfits = getExtremesProfit(newData);
+
+    const aboveAverageProfitDays = getAboveAverageProfitDays(newData);
+    document.getElementById(
+      "highestProfit"
+    ).innerText = `O maior Faturamento foi de ${extremeProfits.highestProfit.toLocaleString(
+      "pt-BR",
+      { style: "currency", currency: "BRL" }
+    )}`;
+    document.getElementById(
+      "lowestProfit"
+    ).innerText = `O menor Faturamento foi de ${extremeProfits.lowestProfit.toLocaleString(
+      "pt-BR",
+      { style: "currency", currency: "BRL" }
+    )}`;
+    document.getElementById(
+      "aboveAverageProfitDays"
+    ).innerText = `A quantidade de dias que tiveram um lucro maior do que a média mensal foram: ${aboveAverageProfitDays}`;
+  })
+  .catch((error) => {
+    console.error("Erro ao ler o arquivo JSON:", error);
+  });
+
+//fourth question
+function getMonthlySum(data) {
+  let TotalSum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    TotalSum += data[i].faturamento;
+  }
+
+  return TotalSum;
+}
+
+function getPercentageEachState(data, totalSum) {
+  let arrPercentageEachState = [];
+
+  data.forEach((state) => {
+    let percentage = (state.faturamento / totalSum).toLocaleString("pt-BR", {
+      style: "percent",
+      minimumFractionDigits: 2,
+    });
+    arrPercentageEachState.push({
+      estado: state.estado,
+      porcentagem: percentage,
+    });
+  });
+
+  return arrPercentageEachState;
+}
+fetch("./faturamentosDistribuidora.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const monthlySum = getMonthlySum(data);
+    const percentageEachState = getPercentageEachState(data, monthlySum);
+    console.log(percentageEachState);
+    document.getElementById(
+      "percentageOfStates"
+    ).innerHTML = `
+    <ul>
+        ${percentageEachState.map(data =>
+      `<li>${data.estado}: ${data.porcentagem}</li>`
+    ).join('')}
+    </ul>`;
+  })
+  .catch((error) => {
+    console.error("Erro ao ler o arquivo JSON:", error);
+  });
 
 function helloFunction() {
-console.log("hello world!")
+  console.log("hello world!");
 }
