f01d561717fb02e0c634daa0462ad9b9214714a8
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-06 22:30:59 -0300

    second question solved

--- index.js
+++ index.js
@@ -17,6 +17,39 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("resultado").innerText = "Portanto o resultado é: " + firstQuestion();
 });
 
+// Segunda questão
+document.getElementById("fibonacciForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário e o recarregamento da página
+
+    const inputElement = document.getElementById("inputNumberToCalcFibonacci");
+    const valueToCalc = inputElement.value;
+
+    calcExistInFibonacci(valueToCalc)
+});
+
+function calcExistInFibonacci(valueReceived) {
+        let prevValue = 0;
+        let currentValue = 1;
+        let fibonacci = 0;
+        let formattedValueReceived = parseInt(valueReceived)
+      
+        while (fibonacci <= formattedValueReceived) {
+          fibonacci = prevValue + currentValue;
+          prevValue = currentValue;
+          currentValue = fibonacci;
+      
+          if (formattedValueReceived === fibonacci || formattedValueReceived === 0) {
+            document.getElementById("secondQuestionResult").innerText = "O número informado pertence à sequência de Fibonacci";
+            return;
+          }
+      
+          if (fibonacci > formattedValueReceived) {
+            document.getElementById("secondQuestionResult").innerText = "O número informado não pertence à sequência de Fibonacci";
+            return;
+          }
+        }
+}
+
 function helloFunction() {
 console.log("hello world!")
 }
